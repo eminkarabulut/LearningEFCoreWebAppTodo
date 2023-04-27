609f2f496dcda50bee6b68d65cc4ee05b44ed43e
Coders <coders@gmail.com>
2023-04-27 18:40:23 +0300

    Add project files.

---
+++ LearningEFCoreWebAppTodo/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using LearningEFCoreWebAppTodo.Entities;
+using LearningEFCoreWebAppTodo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LearningEFCoreWebAppTodo.Controllers
+{
+    public class HomeController : Controller
+    {
+        DatabaseContext db = new DatabaseContext();
+
+        public IActionResult Index()
+        {
+            List<Todo> todos = db.Todos.ToList();
+
+            return View(todos);
+        }
+
+        public IActionResult SampleWhereQuery()
+        {
+            List<Todo> todos = db.Todos.Where(x => x.Category == "work").ToList();
+
+            return View("Index", todos);
+        }
+        
+        public IActionResult SampleLikeQuery()
+        {
+            List<Todo> todos = db.Todos.Where(x => x.Title.Contains("ab")).ToList();
+            //List<Todo> todos = db.Todos.Where(x => x.Title.StartsWith("lorem")).ToList();
+            //List<Todo> todos = db.Todos.Where(x => x.Title.EndsWith("met.")).ToList();
+
+            return View("Index", todos);
+        }
+
+        public IActionResult SampleFirstOrDefaultQuery()
+        {
+            //Todo todo = db.Todos.Where(x => x.Completed == true).FirstOrDefault();
+            Todo todo = db.Todos.FirstOrDefault(x => x.Completed == true);
+
+            return View("Index", new List<Todo> { todo });
+        }
+
+        // LINQ =  Language INtegrated Query (Dile entegre edilmiş sorgulama)
+
+        //public IActionResult Test()
+        //{
+        //    var list = db.Todos.GroupBy(x => x.Category);
+
+        //    foreach (var item in list)
+        //    {
+        //        string key = item.Key;
+        //        int total = item.Sum(x => x.Id);
+        //    }
+            
+
+        //    return View("Index", new List<Todo> { todo });
+        //}
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LearningEFCoreWebAppTodo/Entities/DatabaseContext.cs
@@ -0,0 +1,60 @@
+﻿using MFramework.Services.FakeData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LearningEFCoreWebAppTodo.Entities
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        public DatabaseContext()
+        {
+            // PM> install-package mframework.services.FakeData
+
+            if (Database.EnsureCreated())
+            {
+                string[] categories = new string[] { "work","business","education","shopping","personal","code" };
+
+                for (int i = 0; i < 100; i++)
+                {
+                    Todo todo = new Todo
+                    {
+                        Category = CollectionData.GetElement(categories),
+                        Completed = BooleanData.GetBoolean(),
+                        CreatedAt = DateTimeData.GetDatetime(DateTime.Now.AddYears(-1),DateTime.Now),
+                        Title = TextData.GetSentence()
+                    };
+
+                    Todos.Add(todo);
+                }
+
+                SaveChanges();
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=Z36LearnEFCoreTodoAppDB;Trusted_Connection=true");
+            }
+        }
+    }
+
+    public class Todo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(250)]
+        public string Title { get; set; }
+
+        [StringLength(250)]
+        public string Category { get; set; }
+
+        public bool Completed { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
